3f089599cd974456ad9f16476adf5c21f33c3374
Shannon McNellis <shannmcnelli@gmail.com>
2017-10-29 20:41:22 -0400

    initial commit

---
+++ src/main/java2/com/goodeats/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.goodeats.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+
+
+@Controller
+
+public class HelloController {
+	
+	@org.springframework.web.bind.annotation.RequestMapping(value = "/greeting")
+	public String sayHello(Model model) {
+		
+		model.addAttribute("greeting", "Hello World");
+		
+		return "hello";
+	}
+}
